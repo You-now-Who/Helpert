4f01ae5810707e2311be44d44b2897ba816a6684
You-now-Who <yashyadav.sr@gmail.com>
2023-05-28 20:26:40 +0530

    Update index.jsx

--- sample-01/pages/index.jsx
+++ sample-01/pages/index.jsx
@@ -47,11 +47,11 @@ export default function Index() {
             <div className="place-self-center mr-auto lg:col-span-7">
                 <h1 className="mb-4 max-w-2xl text-4xl font-bold leading-none md:text-5xl xl:text-6xl ">Simplifying College Applications</h1>
                 <p className="mb-6 max-w-2xl text-gray-500 lg:mb-8 md:text-lg lg:text-xl ">From selecting the right colleges to submitting applications, Helpert is here to assist highschool students in streamlining their college application process.</p>
-                <a href="#" className="inline-flex justify-center items-center py-3 px-5 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 ">
+                <a href="/form/list" className="inline-flex justify-center items-center py-3 px-5 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 ">
                     Get started
                     <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
-                <a href="/test" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
+                <a href="/explore" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
                     View colleges
                 </a> 
             </div>
