c2e912b0934f3f75dd66b933fd422a0a42a61893
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-05-28 18:58:09 +0530

    cover-letter
    cover-letter generator

---
+++ sample-01/utils/default-text.js
@@ -0,0 +1,6 @@
+export const coverLetterPropmt = `Compose a professional cover letter demonstrating 
+how my abilities align with the requirements of the university as past description for the Interest at [Desired University.] 
+Use the resume as <resume> as a guide.`;
+export const resume = `
+Paste here a text-only version of your experience, education, and skills.
+`;
